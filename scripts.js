55ac3c8781f691aa244b122d77fd1e5d9edcf909
daesca <reickchozo@gmail.com>
2019-08-19 23:13:58 -0500

    Tratando de agregar la funcionalidad de navegacion normal con scroll despues de dar click a un item del menu

--- scripts.js
+++ scripts.js
@@ -1,6 +1,10 @@
 var activateMenu = document.getElementById("active-menu");
 var menuDynamic = document.getElementById("menu-dynamic");
 
+var container = document.querySelector(".container-content-items");
+var containerElements = document.getElementsByClassName("content-item");
+var cantNegative = 32; //Cantidad de padding inicial
+
 function showOrHiddeMenu(){
     if(!menuDynamic.classList.contains("show")){
         menuDynamic.classList.add("show");
@@ -9,14 +13,41 @@ function showOrHiddeMenu(){
     }
 }
 
-function showOrHiddeElement(elementClicked){
+function showElement(elementClicked){
     
     var element = elementClicked.getAttribute("data-content-item");
     var item = document.getElementById(element);
+    var cantNegative = 32; //Cantidad de padding inicial
+    var i = 0;
 
-    console.log(item);
-    var coordinates = item.offsetTop;
-    console.log(coordinates);
+    while(containerElements[i] != item){
+        cantNegative = cantNegative + containerElements[i].offsetHeight;
+        console.log(cantNegative);
+        i++;
+    }
 
-    document.querySelector(".container-content-items").scrollTo(0, coordinates);
+    container.setAttribute('style', 'top: -' + cantNegative +'px');
+
 }
+
+// window.addEventListener("scroll", function(){
+//     cantNegative = cantNegative - 1;
+//     container.setAttribute('style', 'top: -' + cantNegative +'px');
+    
+// });
+
+// Script para detectar si el scroll sube o baja, tomado de internet
+
+var lastScrollTop = 0;
+// element should be replaced with the actual target element on which you have applied scroll, use window in case of no target element.
+window.addEventListener("scroll", function(){ // or window.addEventListener("scroll"....
+   var st = window.pageYOffset || document.documentElement.scrollTop; // Credits: "https://github.com/qeremy/so/blob/master/so.dom.js#L426"
+   if (st > lastScrollTop){
+        cantNegative = cantNegative - 1;
+        container.setAttribute('style', 'top: -' + cantNegative +'px');
+   } else {
+        cantNegative = cantNegative + 1;
+        container.setAttribute('style', 'top: -' + cantNegative +'px');
+   }
+   lastScrollTop = st <= 0 ? 0 : st; // For Mobile or negative scrolling
+}, false);
