fcdf55510294f9f5619f4327340c0dc5868274cc
daesca <reickchozo@gmail.com>
2019-08-19 23:30:19 -0500

    Agregando tamaño fijo al contenedor de los parrafos y cambiando el objetivo de evaluacion del scroll en javascript

--- scripts.js
+++ scripts.js
@@ -17,7 +17,7 @@ function showElement(elementClicked){
     
     var element = elementClicked.getAttribute("data-content-item");
     var item = document.getElementById(element);
-    var cantNegative = 32; //Cantidad de padding inicial
+    cantNegative = 32; //Cantidad de padding inicial
     var i = 0;
 
     while(containerElements[i] != item){
@@ -40,14 +40,16 @@ function showElement(elementClicked){
 
 var lastScrollTop = 0;
 // element should be replaced with the actual target element on which you have applied scroll, use window in case of no target element.
-window.addEventListener("scroll", function(){ // or window.addEventListener("scroll"....
+document.querySelector(".container-all-document").addEventListener("scroll", function(){ // or window.addEventListener("scroll"....
    var st = window.pageYOffset || document.documentElement.scrollTop; // Credits: "https://github.com/qeremy/so/blob/master/so.dom.js#L426"
    if (st > lastScrollTop){
         cantNegative = cantNegative - 1;
         container.setAttribute('style', 'top: -' + cantNegative +'px');
+        console.log(cantNegative);
    } else {
         cantNegative = cantNegative + 1;
         container.setAttribute('style', 'top: -' + cantNegative +'px');
+        console.log(cantNegative);
    }
    lastScrollTop = st <= 0 ? 0 : st; // For Mobile or negative scrolling
 }, false);
